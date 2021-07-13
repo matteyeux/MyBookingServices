7302d02af5da67d59131220c5b94f3331e38b272
mathieu <mathieu.hautebas@gmail.com>
2021-07-13 16:40:10 +0200

    add basic skeleton for API

--- APIs/booking/booking/app.py
+++ APIs/booking/booking/app.py
@@ -1,18 +1,14 @@
 """Main app module to initialize the FastAPI framework."""
 # from typing import Optional
-import uvicorn
-from fastapi import FastAPI
+from booking.routers import users
+from fastapi import APIRouter
 
 
-app = FastAPI()
+app = APIRouter()
+app.include_router(users.router, tags=["users"])
 
 
 @app.get("/")
 def read_root():
     """Default root."""
     return {"message": "Hello World"}
-
-
-def run_server():
-    """Init API server."""
-    uvicorn.run("booking.app:app", host="127.0.0.1", port=5000, reload=True)
