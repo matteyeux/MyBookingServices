e5c1ba75a62ccdecae6e545a626a3537411cfc16
mathieu <mathieu.hautebas@gmail.com>
2021-07-14 15:41:57 +0200

    skeleton for booking room

--- APIs/booking/booking/routers/book.py
+++ APIs/booking/booking/routers/book.py
@@ -1,9 +1,20 @@
+# from booking.database import Database
 from fastapi import APIRouter
+
 
 router = APIRouter()
 
 
-@router.get("/book/", tags=["book"])
-def book():
+@router.post("/book/{room_id}", tags=["book"])
+async def book_room(room_id: int = 0):
     """Book room"""
-    return {"test": "test"}
+    if room_id <= 0:
+        return None
+
+    return {"room_id": room_id}
+
+
+@router.get("/book/all/", tags=["book"])
+async def booked_rooms():
+    """Get all booked rooms."""
+    return {"booked_rooms": "booked_rooms"}
