5231eca0f9101f7a8a48ef389594a48ffb6c9fcd
mathieu <mathieu.hautebas@gmail.com>
2021-08-17 09:44:02 +0200

    update tests

--- APIs/booking/tests/test_rooms.py
+++ APIs/booking/tests/test_rooms.py
@@ -4,6 +4,68 @@ from fastapi.testclient import TestClient
 client = TestClient(app.app)
 
 
+def test_get_all_rooms():
+    """List all rooms (default)"""
+    expected = {
+        "rooms": [
+            {
+                "id": 1,
+                "hotel_id": 1,
+                "room": "Suite",
+                "price": 720.0,
+                "capacity": 3,
+            },
+            {
+                "id": 2,
+                "hotel_id": 1,
+                "room": "Junior suite",
+                "price": 500.0,
+                "capacity": 2,
+            },
+            {
+                "id": 3,
+                "hotel_id": 1,
+                "room": "Chambre de luxe",
+                "price": 300.0,
+                "capacity": 2,
+            },
+            {
+                "id": 4,
+                "hotel_id": 1,
+                "room": "Chambre standard",
+                "price": 150.0,
+                "capacity": 2,
+            },
+            {
+                "id": 5,
+                "hotel_id": 1,
+                "room": "Chambre standard",
+                "price": 150.0,
+                "capacity": 2,
+            },
+            {
+                "id": 6,
+                "hotel_id": 2,
+                "room": "Suite présidentielle",
+                "price": 1000.0,
+                "capacity": 5,
+            },
+            {
+                "id": 7,
+                "hotel_id": 2,
+                "room": "Suite présidentielle",
+                "price": 1000.0,
+                "capacity": 5,
+            },
+        ],
+    }
+    response = client.get(
+        "/rooms/all/",
+    )
+    assert response.status_code == 200
+    assert response.json() == expected
+
+
 def test_get_available_rooms_01():
     """Test available rooms without capacity."""
     expected = {
@@ -35,28 +97,42 @@ def test_get_available_rooms_01():
         ],
     }
     response = client.get(
-        "/rooms/all/available/?hotel_id=1&start_date=2021-07-21&end_date=2021-07-23",
+        "/rooms/all/available/?hotel_id=1&start_date= \
+        2021-07-21 & end_date=2021-07-23",
     )
     assert response.status_code == 200
     assert response.json() == expected
 
 
-def test_get_available_rooms_02():
-    """Test available rooms with capacity."""
-    expected = {
-        "rooms": [
-            {
-                "id": 1,
-                "room": "Suite",
-                "price": 720,
-                "capacity": 3,
-            },
-        ],
-    }
-
-    response = client.get(
-        "/rooms/all/available/?hotel_id=1&start_date=2021-07-21&end_date=2021-07-23 \
-        &capacity=3",
-    )
-    assert response.status_code == 200
-    assert response.json() == expected
+#
+#
+# def test_get_available_rooms_02():
+#    """Test if dates are invalid."""
+#    expected = {"detail":"Specified dates are invalid"}
+#    response = client.get(
+#        "/rooms/all/available/?hotel_id=1&start_date=2021-07-21&end_date=2021-07-23",
+#    )
+#    assert response.status_code == 400
+#    assert response.json() == expected
+#
+#
+# def test_get_available_rooms_03():
+#    """Test available rooms with capacity."""
+#    expected = {
+#        "rooms": [
+#            {
+#                "id": 1,
+#                "room": "Suite",
+#                "price": 720,
+#                "capacity": 3,
+#            },
+#        ],
+#    }
+#
+#    response = client.get(
+#        "/rooms/all/available/?hotel_id=1&start_date= \
+#        2021-07-21&end_date=2021-07-23 \
+#        &capacity=3",
+#    )
+#    assert response.status_code == 200
+#    assert response.json() == expected
