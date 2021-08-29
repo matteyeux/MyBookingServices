6596faa05d8b82546da0fc40b1be4b9ccf05c035
yoann <humeau_y@etna-alternance.net>
2021-08-29 13:40:49 -0700

    It works on my machine

--- APIs/users/users/app.py
+++ APIs/users/users/app.py
@@ -1,9 +1,9 @@
 """Main app module to initialize the FastAPI framework."""
-from users.routers import user
+from users.routers import users
 from fastapi import FastAPI
 
 app = FastAPI()
-app.include_router(user.router, tags=["users"])
+app.include_router(users.router, tags=["users"])
 
 
 @app.get("/")
