c39b654ba11d640003ded2cf309a6254b613950d
Alexandre <majda_a@etna-alternance.net>
2021-08-31 11:50:45 +0200

    [Feature] Adding options router.

---
+++ APIs/management/management/routers/options.py
@@ -0,0 +1,38 @@
+from management.models.options import Options
+from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Optional
+
+# from fastapi import Request
+
+router = APIRouter()
+
+
+class Option(BaseModel):
+    name: str
+    price: float
+
+@router.get("/options/all/", tags=["options"])
+async def get_options():
+    """ Get all options."""
+    options = Options().get_all_options()
+
+    return {"options": options}
+
+@router.get("/options/{option_id}", tags=["options"])
+async def get_option_by_id(option_id: int = 1):
+    """ Get one option by its id. """
+    option = Options().get_option_by_id(option_id)
+    return {"option": option}
+
+@router.post("/options", tags=["options"])
+async def add_new_option(option):
+    """ Add a new option"""
+    option = Options().add_option(option)
+    return {"option": option}
+
+@router.put("/options/{option_id}", tags=["options"])
+async def update_option(option, option_id: int = 1):
+    """ Update option's price """
+    option = Options().update_option(option, option_id)
+    return {"option": option}
