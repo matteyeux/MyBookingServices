20e65acebdbc3edeee3eeca11e8fb070e00ea804
Alexandre <majda_a@etna-alternance.net>
2021-08-31 11:51:10 +0200

    [Feature] Include option's router.

--- APIs/management/management/app.py
+++ APIs/management/management/app.py
@@ -1,11 +1,13 @@
 """Main app module to initialize the FastAPI framework."""
 from management.routers import hotels
 from management.routers import rooms
+from management.routers import options
 from fastapi import FastAPI
 
 app = FastAPI()
 app.include_router(hotels.router, tags=["hotels"])
 app.include_router(rooms.router, tags=["rooms"])
+app.include_router(options.router, tags=["options"])
 
 
 @app.get("/")
