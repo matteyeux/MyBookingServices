dacf8a3eaab4742033ae35c246813f49ed04bcff
Alexandre <majda_a@etna-alternance.net>
2021-09-01 11:21:18 +0200

    [Feature] Adding delete route. Fixing argument handling for insert, update routes.

--- APIs/management/management/routers/options.py
+++ APIs/management/management/routers/options.py
@@ -26,13 +26,19 @@ async def get_option_by_id(option_id: int = 1):
     return {"option": option}
 
 @router.post("/options", tags=["options"])
-async def add_new_option(option):
+async def add_new_option(option: Option):
     """ Add a new option"""
     option = Options().add_option(option)
     return {"option": option}
 
 @router.put("/options/{option_id}", tags=["options"])
-async def update_option(option, option_id: int = 1):
+async def update_option(option: Option, option_id: int = 1):
     """ Update option's price """
     option = Options().update_option(option, option_id)
     return {"option": option}
+
+@router.delete("/options/{option_id}", tags=["options"])
+async def delete_option(option_id: int = 0):
+    if option_id > 0:
+        option = Options().delete_option(option_id)
+        return "ok"
