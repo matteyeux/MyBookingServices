3fcb545bee814f880666cbd48cd62f07235007c4
Alexandre <majda_a@etna-alternance.net>
2021-09-05 14:37:29 +0200

    [Fix] Fixing Typo.

--- APIs/management/management/app.py
+++ APIs/management/management/app.py
@@ -2,14 +2,14 @@
 from management.routers import hotels
 from management.routers import rooms
 from management.routers import options
-from management.routers import price_policy
+from management.routers import price_policies
 from fastapi import FastAPI
 
 app = FastAPI()
 app.include_router(hotels.router, tags=["hotels"])
 app.include_router(rooms.router, tags=["rooms"])
 app.include_router(options.router, tags=["options"])
-app.include_router(price_policy.router, tags=["price_policy"])
+app.include_router(price_policies.router, tags=["price_policy"])
 
 
 @app.get("/")
