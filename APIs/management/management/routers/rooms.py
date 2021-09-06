a4c80b2668a2aa9a36b1149b2bf10f3d1321c57b
Alexandre <majda_a@etna-alternance.net>
2021-09-06 22:31:51 +0200

    [Feature] Adding post, put, delete and last route for rooms.

--- APIs/management/management/routers/rooms.py
+++ APIs/management/management/routers/rooms.py
@@ -1,16 +1,35 @@
-from management.models.rooms import Rooms
-from management.utils import check_dates
+from typing import Optional
+
 from fastapi import APIRouter
 from fastapi import HTTPException
+from management.models.rooms import Rooms
+from management.utils import check_dates
+from pydantic import BaseModel
 
-# from fastapi import Request
 
 router = APIRouter()
+
+
+class Room(BaseModel):
+    hotel_id: Optional[int] = 1
+    room: str
+    price: int
+    capacity: int
+
+
+@router.get("/rooms/all/", tags=["rooms"])
+async def get_all_rooms(hotel_id: int = 0, capacity: int = 0):
+    """Route to list all rooms."""
+
+    rooms = Rooms()
+    all_rooms = rooms.get_all_rooms(hotel_id, capacity)
+    return {"rooms": all_rooms}
 
 
 @router.get("/rooms/{room_id}", tags=["rooms"])
 async def get_room_info_by_id(room_id: int = 0):
     """Get info about room"""
+
     if room_id <= 0:
         raise HTTPException(status_code=400, detail="Can't use id <= 0")
     rooms = Rooms()
@@ -18,15 +37,16 @@ async def get_room_info_by_id(room_id: int = 0):
 
     if not room:
         raise HTTPException(status_code=404, detail="Room not found")
-    return {"room_id": room}
+    return {"room": room}
 
 
-# @router.get("/rooms/test/")
-# async def do_test(param1: str, param2: str):
-# #async def do_test(request: Request):
-#     print(param2)
-#     return {"test"}
-#     #return await request.json()
+@router.get("/rooms/last/", tags=["rooms"])
+async def get_last_room():
+    """Route to list all rooms."""
+
+    rooms = Rooms().get_all_rooms()
+    last_room = rooms[-1]
+    return {"room": last_room}
 
 
 @router.get("/rooms/all/available/", tags=["rooms"])
@@ -54,10 +74,33 @@ async def get_available_rooms(
     return {"rooms": available_rooms}
 
 
-@router.get("/rooms/all/", tags=["rooms"])
-async def get_all_rooms(hotel_id: int = 0, capacity: int = 0):
-    """Route to list all rooms."""
-    rooms = Rooms()
-    all_rooms = rooms.get_all_rooms(hotel_id, capacity)
+@router.post("/rooms", tags=["rooms"])
+async def create_room(room: Room):
+    """ Create a new room in a hotel. """
 
-    return {"rooms": all_rooms}
+    room = Rooms().create_room(room)
+    return {"room": room}
+
+
+@router.put("/rooms/{room_id}", tags=["rooms"])
+async def update_room(room: Room, room_id: int = 1):
+    """ Update a room by its id. """
+
+    if not Rooms().get_room_by_id(room_id):
+        raise HTTPException(status_code=404, detail="Room not found")
+
+    room = Rooms().update_room(room, room_id)
+    return {"room": room}
+
+
+@router.delete("/rooms/{room_id}", tags=["rooms"])
+async def delete_room(room_id: int = 0):
+    """ Delete a room by its id. """
+
+    if room_id > 0:
+        room = Rooms().delete_room(room_id)
+
+        if not room:
+            raise HTTPException(status_code=404, detail="Room not found")
+
+        return room
